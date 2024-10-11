1c66cf10a2648a0fd55d63fbc5a3c7d3b0f6d5bf
vishnubansaltarento <vishnu.bansal@tarento.com>
2024-10-11 16:08:35 +0530

    update for vapt-issue-fix

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+logger.error({ message: "Cookie Issue" });
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
