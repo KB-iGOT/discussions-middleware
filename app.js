a8c1c5dd431253256dbe62de585e5f81e0fe9610
vishnubansaltarento <vishnu.bansal@tarento.com>
2024-10-11 15:13:45 +0530

    update for vapt-issue-fix

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var nodebb = require('./routes/nodebb');
+var cors = require('cors');
+var app = express();
+const telemetry = new (require('./libs/sb_telemetry_util/telemetryService'))()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -13,21 +21,12 @@ app.use(cors());
 app.all('/health', (req,res,next) => {
   res.send({statusCode: 200, message: "health api"})
 })
-
-app.get('/', (req,res)=>{
-  //.....Other Code
-  res.cookie('cookieName', 'cookieValue', { sameSite: 'none', secure: true})
-  //.....Other Code
-})
-
 app.use('/', nodebb);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-
 
 // error handler
 app.use(function(err, req, res, next) {
